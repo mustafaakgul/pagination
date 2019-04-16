2f611b4de7bf1d743da6c8dc2828e475503beb5a
mustafaakgul <mustafaakgul@yandex.com>
2019-04-16 20:30:21 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @RequestMapping(value = "/listPageable", method = RequestMethod.GET)
+    Page<Post> postsPageable(Pageable pageable) {
+        return postRepository.findAll(pageable);
+
+    }
+}
---
+++ src/main/java/com/example/demo/model/Post.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private long id;
+
+    private String name;
+
+    private String tag;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
